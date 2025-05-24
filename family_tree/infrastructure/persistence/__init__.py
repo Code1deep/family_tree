f251a3c2af627b8560a8e39a6ce68c5f01024471
Code1deep <az.bahya@gmail.com>
2025-05-24 19:08:11 -0400

    Update __init__.py

--- family_tree/infrastructure/persistence/__init__.py
+++ family_tree/infrastructure/persistence/__init__.py
@@ -1,5 +1,5 @@
 
-from app.extensions import db  # Utilisez le db centralisé
-from domain.models.person import Person
+from family_tree.app.extensions import db  # Utilisez le db centralisé
+from family_tree.domain.models.person import Person
 
 __all__ = ["Person"]  # db retiré car importé depuis extensions
