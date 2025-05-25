a06ae3cf25ffcc6afb3733f0205d8353ae2cf45d
Code1deep <az.bahya@gmail.com>
2025-05-25 16:13:44 -0400

    Update init_person_service.py

--- family_tree/interfaces/api/resources/person/init_person_service.py
+++ family_tree/interfaces/api/resources/person/init_person_service.py
@@ -5,17 +5,17 @@ from family_tree.infrastructure.persistence.repositories.person_repo import Pers
 from family_tree.app.extensions import db
 
 person_service = None
-init_resources(db.session)
 
 def init_resources(db_session):
     global person_service
-    from family_tree.infrastructure.persistence.repositories.person_repo import PersonRepository
     repo = PersonRepository(db_session)
     person_service = PersonService(repo)
 
 # ------------ INIT POUR INJECTION DE SERVICE (tests) -----------
-
 def create_person_api(service):
     global person_service
     person_service = service
     return person_api
+
+# ⚠️ Cet appel doit venir *après* la définition
+init_resources(db.session)
