c234e20d5d3d149f94289b605f7871865242e1bb
Code1deep <az.bahya@gmail.com>
2025-05-25 15:45:57 -0400

    Update routes_api.py

--- family_tree/interfaces/api/resources/person/routes_api.py
+++ family_tree/interfaces/api/resources/person/routes_api.py
@@ -5,20 +5,20 @@ from family_tree.interfaces.api.resources.person.init_person_service import pers
 from family_tree.interfaces.api.serializers.person_serializer import PersonSerializer
 from family_tree.domain.models import Person  # ← Nécessaire pour les requêtes dans search_persons()
 
-def register_api_routes(bp):
-    @bp.route('/api/person/<int:person_id>')
+def register_api_routes(person_api):
+    @person_api.route('/api/person/<int:person_id>')
     def get_person_by_id(person_id):
         person = person_service.get_by_id(person_id)
         if not person:
             abort(404, "Person not found")
         return jsonify(person)
 
-    @bp.route('/api/person/<int:person_id>/children')
+    @person_api.route('/api/person/<int:person_id>/children')
     def get_person_children(person_id):
         children = person_service.get_children(person_id)
         return jsonify([PersonSerializer.serialize_basic(c) for c in children])
 
-    @bp.route('/api/person')
+    @person_api.route('/api/person')
     def get_person():
         person_id = request.args.get('id', type=int)
         if not person_id:
@@ -32,7 +32,7 @@ def register_api_routes(bp):
         except ValueError as e:
             abort(404, str(e))
 
-    @bp.route('/api/persons', methods=['GET'])
+    @person_api.route('/api/persons', methods=['GET'])
     def search_persons():
         name = request.args.get('name')
         gender = request.args.get('gender')
