8cb3ac8308e3953881c2ed314e6fed6960442656
Code1deep <az.bahya@gmail.com>
2025-05-26 19:12:49 -0400

    Update init_tree_service.py

--- family_tree/interfaces/api/resources/tree/init_tree_service.py
+++ family_tree/interfaces/api/resources/tree/init_tree_service.py
@@ -1,7 +1,7 @@
 # interfaces/api/resources/tree/init_tree_service.py
 
-from family_tree.interfaces.api.resources.tree import tree_api
-
+# from family_tree.interfaces.api.resources.tree import tree_api
 def init_tree_resources(app):
     """Initialise les ressources de l'arbre"""
+    from . import tree_api  # Import local pour éviter les circulaires
     app.register_blueprint(tree_api, url_prefix='/tree')
