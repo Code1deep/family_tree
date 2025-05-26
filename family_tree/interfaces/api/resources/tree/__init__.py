8a38182a14b2d4782145e608ff34c522ce2d0e91
Code1deep <az.bahya@gmail.com>
2025-05-26 19:09:22 -0400

    Update __init__.py

--- family_tree/interfaces/api/resources/tree/__init__.py
+++ family_tree/interfaces/api/resources/tree/__init__.py
@@ -1,4 +1,4 @@
-# interfaces/api/resources/tree/_init__.py
+# interfaces/api/resources/tree/__init__.py
 from flask import Blueprint
 from .routes_tree_api import create_tree_api
 from .routes_tree_views import register_tree_views
