8dbc3015f559cb49efe79affdc7bfa30f4bb6fd6
Code1deep <az.bahya@gmail.com>
2025-05-25 22:26:01 -0400

    Create utils.js

---
+++ family_tree/static/js/tree/utils.js
@@ -0,0 +1,52 @@
+// static/js/tree/utils.js
+export function centerTree(svg, container) {
+    const bounds = svg.node().getBBox();
+    const fullWidth = container.clientWidth;
+    const fullHeight = window.innerHeight * 0.8;
+    const translateX = (fullWidth - bounds.width) / 2 - bounds.x;
+    const translateY = (fullHeight - bounds.height) / 2 - bounds.y;
+    svg.transition().duration(500).attr("transform", `translate(${translateX},${translateY})`);
+}
+
+export function exportAsPNG() {
+    const svgEl = document.querySelector("#tree-container svg");
+    const svgData = new XMLSerializer().serializeToString(svgEl);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    
+    canvas.width = svgEl.viewBox.baseVal.width;
+    canvas.height = svgEl.viewBox.baseVal.height;
+    
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+        const png = canvas.toDataURL("image/png");
+        downloadFile(png, "arbre.png");
+    };
+    
+    img.src = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgData)));
+}
+
+export function exportAsSVG() {
+    const svgEl = document.querySelector("#tree-container svg");
+    const svgData = new XMLSerializer().serializeToString(svgEl);
+    const blob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    const url = URL.createObjectURL(blob);
+    downloadFile(url, "arbre.svg");
+    URL.revokeObjectURL(url);
+}
+
+export function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch(console.error);
+    } else {
+        document.exitFullscreen();
+    }
+}
+
+function downloadFile(url, filename) {
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+}
