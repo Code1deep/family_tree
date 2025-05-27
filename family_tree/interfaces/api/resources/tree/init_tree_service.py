99bc9fd0af97cf7fb8d7e0feec82551eb9bc9ea4
Code1deep <az.bahya@gmail.com>
2025-05-26 21:33:26 -0400

    Update init_tree_service.py

--- family_tree/interfaces/api/resources/tree/init_tree_service.py
+++ family_tree/interfaces/api/resources/tree/init_tree_service.py
@@ -1,5 +1,16 @@
 # interfaces/api/resources/tree/init_tree_service.py
 
+from flask import Blueprint
+
+def create_tree_api(app, person_service):
+    tree_api = Blueprint("tree_api", __name__)
+
+    @tree_api.route("/tree")
+    def tree_view():
+        return "Tree API working"
+
+    app.register_blueprint(tree_api)
+
 def init_tree_resources(app, person_service):
     """Initialise les ressources de l'arbre avec service injecté"""
     from .routes_tree_api import create_tree_api  # Import local
