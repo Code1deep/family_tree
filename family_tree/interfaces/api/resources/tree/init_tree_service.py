64738454ac14af37bd01d983e64584b423814cd5
Code1deep <az.bahya@gmail.com>
2025-05-26 20:14:02 -0400

    Update init_tree_service.py

--- family_tree/interfaces/api/resources/tree/init_tree_service.py
+++ family_tree/interfaces/api/resources/tree/init_tree_service.py
@@ -1,7 +1,8 @@
 # interfaces/api/resources/tree/init_tree_service.py
 
-# from family_tree.interfaces.api.resources.tree import tree_api
-def init_tree_resources(app):
-    """Initialise les ressources de l'arbre"""
-    from . import tree_api  # Import local pour éviter les circulaires
+def init_tree_resources(app, person_service):
+    """Initialise les ressources de l'arbre avec service injecté"""
+    from .routes_tree_api import create_tree_api  # Import local
+    tree_api = create_tree_api(person_service)
     app.register_blueprint(tree_api, url_prefix='/tree')
+
