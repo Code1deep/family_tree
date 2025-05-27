90fe0599c5f8459e1d72041efbb098087c1bfb93
Code1deep <az.bahya@gmail.com>
2025-05-26 22:15:09 -0400

    Update __init__.py

--- family_tree/interfaces/api/resources/tree/__init__.py
+++ family_tree/interfaces/api/resources/tree/__init__.py
@@ -1,15 +1,5 @@
 # interfaces/api/resources/tree/__init__.py
 
-from flask import Blueprint
-
-# Création du blueprint
-tree_api = Blueprint('tree_api', __name__)
-
-# Import des routes APRÈS la création du blueprint pour éviter les imports circulaires
-#from . import routes_tree_api
 from .init_tree_service import init_tree_resources
-
-# Initialisation des routes
-# routes_tree_api.register_routes(tree_api)
 
 
