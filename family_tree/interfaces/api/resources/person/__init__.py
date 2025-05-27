3e103e448f85fb7c3807aa871d5b536ea9fa9ed5
Code1deep <az.bahya@gmail.com>
2025-05-27 05:33:45 -0400

    Update __init__.py

--- family_tree/interfaces/api/resources/person/__init__.py
+++ family_tree/interfaces/api/resources/person/__init__.py
@@ -1,17 +1,17 @@
 # interfaces/api/resources/person/__init__.py
 
-from family_tree.interfaces.api.resources.person.blueprint import person_api
 from family_tree.interfaces.api.resources.person.routes_crud import register_crud_routes
 from family_tree.interfaces.api.resources.person.routes_api import register_api_routes
 from family_tree.interfaces.api.resources.person.routes_tree import register_tree_routes
 from family_tree.interfaces.api.resources.person.routes_misc import register_misc_routes
+from family_tree.interfaces.api.resources.person.blueprint import person_api
 
-# Enregistrement des routes
 register_crud_routes(person_api)
 register_api_routes(person_api)
 register_tree_routes(person_api)
 register_misc_routes(person_api)
 
 __all__ = ['person_api']
+
 
 
