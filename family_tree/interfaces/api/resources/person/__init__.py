983db61f3c79309da360d0ad876fe7e8f4979a25
Code1deep <az.bahya@gmail.com>
2025-05-27 00:07:14 -0400

    Update __init__.py

--- family_tree/interfaces/api/resources/person/__init__.py
+++ family_tree/interfaces/api/resources/person/__init__.py
@@ -1,13 +1,10 @@
 # interfaces/api/resources/person/__init__.py
 
-from flask import Blueprint
+from family_tree.interfaces.api.resources.person.blueprint import person_api
 from family_tree.interfaces.api.resources.person.routes_crud import register_crud_routes
 from family_tree.interfaces.api.resources.person.routes_api import register_api_routes
 from family_tree.interfaces.api.resources.person.routes_tree import register_tree_routes
 from family_tree.interfaces.api.resources.person.routes_misc import register_misc_routes
-
-# Création du blueprint
-person_api = Blueprint('person_api', __name__)
 
 # Enregistrement des routes
 register_crud_routes(person_api)
@@ -15,6 +12,6 @@ register_api_routes(person_api)
 register_tree_routes(person_api)
 register_misc_routes(person_api)
 
-# Export explicite
 __all__ = ['person_api']
+
 
