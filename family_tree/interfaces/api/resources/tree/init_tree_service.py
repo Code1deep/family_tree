abf52015895ffc74c58619875b36b0630423a103
Code1deep <az.bahya@gmail.com>
2025-05-27 13:42:40 -0400

    Update init_tree_service.py

--- family_tree/interfaces/api/resources/tree/init_tree_service.py
+++ family_tree/interfaces/api/resources/tree/init_tree_service.py
@@ -3,6 +3,7 @@
 from flask import Blueprint
 from .routes_tree_api import register_tree_api_routes
 from .routes_tree_views import register_tree_view_routes
+from family_tree.interfaces.api.resources.person.routes_tree import register_tree_routes, inject_service
 
 def init_tree_resources(app, person_service):
     """
@@ -10,6 +11,10 @@ def init_tree_resources(app, person_service):
     - Un blueprint pour les routes HTML (interface utilisateur)
     - Un blueprint pour les routes API JSON (backend)
     """
+    inject_service(person_service)  # Injecte le service dans routes_tree
+    tree_api = Blueprint('tree_api', __name__)
+    register_tree_routes(tree_api)
+    app.register_blueprint(tree_api)
 
     # 🧩 Blueprint pour les vues HTML (ex : /tree/, /tree/1)
     tree_html_bp = Blueprint("tree_html", __name__)
