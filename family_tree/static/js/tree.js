cea2a2b18be2ff51821bc6c4da4dd586fb36f007
Code1deep <az.bahya@gmail.com>
2025-05-29 13:09:49 -0400

    Update tree.js

--- family_tree/static/js/tree.js
+++ family_tree/static/js/tree.js
@@ -2,12 +2,12 @@
 <script src="https://balkan.app/js/OrgChart.js"></script>
 
 import { loadTreeData } from './tree/core.js';
-import { renderFamilyTree } from './tree/d3-tree.js';
+import { initD3Tree } from './tree/d3-tree.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         const treeData = await loadTreeData(1); // Charge les données
-        renderFamilyTree(treeData); // Affiche l'arbre
+        initD3Tree(treeData); // Affiche l'arbre
     } catch (error) {
         console.error("Erreur:", error);
         alert("Erreur de chargement de l'arbre");
