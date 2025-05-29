8dfacf275e789c40085fc4205b0da89d574db2f8
Code1deep <az.bahya@gmail.com>
2025-05-29 13:07:08 -0400

    Update d3-tree.js

--- family_tree/static/js/tree/d3-tree.js
+++ family_tree/static/js/tree/d3-tree.js
@@ -2,7 +2,7 @@
 import { initControls } from './controls.js';
 import { centerTree, exportAsPNG, exportAsSVG, toggleFullscreen } from './utils.js';
 
-export function renderFamilyTree(data) {
+export function initD3Tree(data) {
     console.log("Rendering tree with data:", data);
     const container = document.getElementById("tree-container");
     const margin = { top: 50, right: 90, bottom: 30, left: 90 };
