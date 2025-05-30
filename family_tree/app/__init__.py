2d1815b70af1d02349c090d7b1b25a956d445ebf
Code1deep <az.bahya@gmail.com>
2025-05-30 13:59:33 -0400

    Update __init__.py

--- family_tree/app/__init__.py
+++ family_tree/app/__init__.py
@@ -1,6 +1,10 @@
 # family_tree/app/__init__.py
 from flask import app, current_app, request
 from .factory import create_app
+import logging.config
+from config import LOGGING_CONFIG
+logging.config.dictConfig(LOGGING_CONFIG)
+
 @app.before_request
 def log_request_info():
     current_app.logger.info(f"[ROUTE] {request.method} {request.path}")
