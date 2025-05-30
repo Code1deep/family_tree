919fee6e90bf971a5d1a202d73f1d725700a2ecf
Code1deep <az.bahya@gmail.com>
2025-05-30 14:41:56 -0400

    Update __init__.py

--- family_tree/app/__init__.py
+++ family_tree/app/__init__.py
@@ -5,16 +5,6 @@ import logging.config
 from family_tree.app.config import LOGGING_CONFIG
 logging.config.dictConfig(LOGGING_CONFIG)
 
-@app.before_request
-def log_request_info():
-    current_app.logger.info(f"[ROUTE] {request.method} {request.path}")
-    if request.json:
-        current_app.logger.info(f"[BODY] {request.json}")
-    if request.args:
-        current_app.logger.info(f"[ARGS] {request.args}")
-    if request.form:
-        current_app.logger.info(f"[FORM] {request.form}")
-
 @app.after_request
 def log_response_info(response):
     current_app.logger.info(f"[RESPONSE] {response.status} {response.content_type}")
