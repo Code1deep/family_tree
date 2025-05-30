157cf8c4fe5f774f4528d51fb87b082fb797669f
Code1deep <az.bahya@gmail.com>
2025-05-30 14:43:29 -0400

    Update __init__.py

--- family_tree/app/__init__.py
+++ family_tree/app/__init__.py
@@ -1,11 +1,2 @@
 # family_tree/app/__init__.py
-from flask import Flask, app, current_app, request
-from family_tree.app.factory import create_app
-import logging.config
-from family_tree.app.config import LOGGING_CONFIG
-logging.config.dictConfig(LOGGING_CONFIG)
-
-@app.after_request
-def log_response_info(response):
-    current_app.logger.info(f"[RESPONSE] {response.status} {response.content_type}")
-    return response
+from .factory import create_app
