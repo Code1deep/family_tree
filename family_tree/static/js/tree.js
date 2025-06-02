d8f0a246914bcc1c23eb6889834eeb4543543acb
Code1deep <az.bahya@gmail.com>
2025-06-01 20:08:26 -0400

    Update tree.js

--- family_tree/static/js/tree.js
+++ family_tree/static/js/tree.js
@@ -1,7 +1,8 @@
 // static/js/tree.js
 import { loadTreeData } from './tree/core.js';
 import { initD3Tree } from './tree/d3-tree.js';
-import { initTree, zoomIn, zoomOut, centerTree, exportPNG, exportSVG, searchNode } from './tree/core.js';
+import { initTree, zoomIn, zoomOut, exportPNG, exportSVG, searchNode } from './tree/core.js';
+import { centerTree } from './tree/utils.js'; 
 import { openModal } from "/static/js/modal.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
