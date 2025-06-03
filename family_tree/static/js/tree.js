93419b3749a44ed5ac418422bcb1fd78a01c7d68
Code1deep <az.bahya@gmail.com>
2025-06-03 03:38:38 -0400

    Update tree.js

--- family_tree/static/js/tree.js
+++ family_tree/static/js/tree.js
@@ -6,10 +6,10 @@ import { openModal } from "/static/js/modal.js";
 import { initMainD3Tree, initSubD3Tree } from './tree/index.js';
 
 console.log('✅ tree.js loaded');
-console.log('treeData:', typeof treeData !== 'undefined' ? treeData : 'NOT DEFINED');
+console.log('data:', typeof data !== 'undefined' ? data : 'NOT DEFINED');
 
 // Initialisation directe si les données sont déjà disponibles
-initSubD3Tree("modal-tree-container", treeData);
+initSubD3Tree("modal-tree-container", data);
 
 // Facultatif : exposer pour debug
 window.initD3Tree = initMainD3Tree;
@@ -24,9 +24,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   try {
     const response = await fetch("/api/tree/");
     if (!response.ok) throw new Error(`Erreur HTTP ${response.status}`);
-    const treeData = await response.json();
-    console.log("✅ Données reçues :", treeData);
-    initMainD3Tree("tree-container", treeData);
+    const data = await response.json();
+    console.log("✅ Données reçues :", data);
+    initMainD3Tree("tree-container", data);
   } catch (err) {
     console.error("❌ Erreur lors du chargement de l’arbre :", err);
   }
