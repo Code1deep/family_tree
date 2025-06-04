3247644660687b5b4a8f4693d3b7c6a86f75de37
Code1deep <az.bahya@gmail.com>
2025-06-04 09:52:51 -0400

    Update core.js

--- family_tree/static/js/tree/core.js
+++ family_tree/static/js/tree/core.js
@@ -1,5 +1,5 @@
 // static/js/tree/core.js
-import * as d3 from 'https://d3js.org/d3.v7.min.js';
+//import * as d3 from 'https://d3js.org/d3.v7.min.js';
 import { transformDataForD3 } from './d3-tree.js';
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import { centerTree, exportTreeAsPNG, exportTreeAsSVG, toggleFullscreen } from './utils.js';
