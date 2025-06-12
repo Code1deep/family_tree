dbe50efc782c06ddcec2844a4d3a7c9eaed166df
Code1deep <az.bahya@gmail.com>
2025-06-12 14:52:46 -0400

    Create user.py

---
+++ family_tree/interfaces/auth/user.py
@@ -0,0 +1,16 @@
+# interfaces/auth/user.py
+from flask_login import UserMixin
+from family_tree.app.extensions import login_manager
+
+# Simuler un utilisateur
+users = {'foo@bar.com': {'password': 'secret'}}
+
+class User(UserMixin):
+    def __init__(self, user_id=None):
+        self.id = user_id  # <-- ici on définit l'attribut proprement
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id not in users:
+        return None
+    return User(user_id)
