b49bbbcba6184b7c0b15c42c8d722b6c68e90d32
Code1deep <az.bahya@gmail.com>
2025-06-17 06:46:58 -0400

    Update tree.js

--- family_tree/static/js/tree/tree.js
+++ family_tree/static/js/tree/tree.js
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     document.getElementById("pngBtn")?.addEventListener("click", () => {
         console.log("📷 Clic bouton : Export PNG");
-        exportAsPNG(treeContainer);
+        exportAsPNG("wrapper");
     });
 
     document.getElementById("svgBtn")?.addEventListener("click", () => {
