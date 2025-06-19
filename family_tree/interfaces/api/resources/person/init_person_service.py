d90f6e4c614d589b4f83a9dc797c995a960d48fd
Code1deep <az.bahya@gmail.com>
2025-06-18 21:02:41 -0400

    Update init_person_service.py

--- family_tree/interfaces/api/resources/person/init_person_service.py
+++ family_tree/interfaces/api/resources/person/init_person_service.py
@@ -1,21 +1,21 @@
 # interfaces/api/resources/person/init_person_service.py
 from family_tree.domain.services.person_service import PersonService
 from family_tree.infrastructure.persistence.repositories.person_repo import PersonRepository
-
-# Import des injecteurs uniquement (pas de blueprint ici)
-from family_tree.interfaces.api.resources.person.routes_api import inject_service as inject_api
-from family_tree.interfaces.api.resources.person.routes_crud import inject_service as inject_crud
-from family_tree.interfaces.api.resources.person.routes_tree import inject_service as inject_tree
-from family_tree.interfaces.api.resources.person.routes_misc import inject_service as inject_misc
-
+from family_tree.interfaces.api.resources.person.routes_crud import register_crud_routes
+from family_tree.interfaces.api.resources.person.routes_api import register_api_routes
+from family_tree.interfaces.api.resources.person.routes_tree import register_tree_routes
+from family_tree.interfaces.api.resources.person.routes_misc import register_misc_routes
 from family_tree.interfaces.api.resources.person.blueprint import person_api
 
 def init_person_resources(db_session):
     service = PersonService(PersonRepository(db_session))
-    inject_api(service)
-    inject_crud(service)
-    inject_tree(service)
-    inject_misc(service)
+    
+    # Tu passes le service explicitement partout
+    register_crud_routes(person_api, service)
+    register_api_routes(person_api, service)
+    register_tree_routes(person_api, service)
+    register_misc_routes(person_api, service)
 
 def create_person_api():
     return person_api
+
