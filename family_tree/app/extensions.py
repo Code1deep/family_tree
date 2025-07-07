c40efe6d79ffc260d93b37c53518a55ac8999b2e
Code1deep <az.bahya@gmail.com>
2025-07-07 13:28:51 -0400

    Update extensions.py

--- family_tree/app/extensions.py
+++ family_tree/app/extensions.py
@@ -1,16 +1,13 @@
 # \app\extensions.py
+from setup_path import setup_sys_path
+setup_sys_path(__file__)
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_babel import Babel
 from flask_migrate import Migrate
-from huggingface_hub import User
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 babel = Babel()
 migrate = Migrate()
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
