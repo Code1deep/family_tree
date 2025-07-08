5bc6ec057a7139d94a3c77f5d18d621d27f436f6
Code1deep <az.bahya@gmail.com>
2025-07-07 21:34:26 -0400

    Update create_persons.py

--- family_tree/create_persons.py
+++ family_tree/create_persons.py
@@ -1,5 +1,4 @@
 # family_tree/create_persons.py
-
 from family_tree.app.extensions import db
 from family_tree.domain.models.person import Person
 from sqlalchemy import inspect
@@ -7,8 +6,11 @@ from sqlalchemy import inspect
 def create_persons_table():
     inspector = inspect(db.engine)
     if not inspector.has_table(Person.__tablename__):
-        db.create_all()
-        print(f"✅ Table '{Person.__tablename__}' créée.")
+        print("✅ Table 'persons' absente, création...")
+        with db.engine.begin() as conn:
+            db.create_all()
+        print("✅ Table 'persons' créée et transaction validée.")
     else:
-        print(f"ℹ️ Table '{Person.__tablename__}' existe déjà.")
+        print("✅ Table 'persons' déjà présente.")
+
 
