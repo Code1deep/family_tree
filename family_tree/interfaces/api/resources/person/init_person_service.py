6fd7119abbd8856170b80d58607eb6621720a1cf
Code1deep <az.bahya@gmail.com>
2025-07-08 18:15:21 -0400

    Update init_person_service.py

--- family_tree/interfaces/api/resources/person/init_person_service.py
+++ family_tree/interfaces/api/resources/person/init_person_service.py
@@ -1,15 +1,15 @@
 # interfaces/api/resources/person/init_person_service.py
-from domain.services.person_service import PersonService
+from family_tree.domain.services.person_service import PersonService
 
-from infrastructure.persistence.repositories.person_repo import PersonRepository
+from family_tree.infrastructure.persistence.repositories.person_repo import PersonRepository
 
 # Import des injecteurs uniquement
-from interfaces.api.resources.person.routes_api import inject_service as inject_api, register_api_routes
-from interfaces.api.resources.person.routes_crud import inject_service as inject_crud, register_crud_routes
-from interfaces.api.resources.person.routes_tree import inject_service as inject_tree, register_tree_routes
-from interfaces.api.resources.person.routes_misc import inject_service as inject_misc, register_misc_routes
+from family_tree.interfaces.api.resources.person.routes_api import inject_service as inject_api, register_api_routes
+from family_tree.interfaces.api.resources.person.routes_crud import inject_service as inject_crud, register_crud_routes
+from family_tree.interfaces.api.resources.person.routes_tree import inject_service as inject_tree, register_tree_routes
+from family_tree.interfaces.api.resources.person.routes_misc import inject_service as inject_misc, register_misc_routes
 
-from interfaces.api.resources.person.blueprint import person_api
+from family_tree.interfaces.api.resources.person.blueprint import person_api
 
 def init_person_resources(db_session):
     # Création d'une seule instance de service
