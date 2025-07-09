5053a72ade868bc7d0bfaa7fcafc8a2d6188726d
Code1deep <az.bahya@gmail.com>
2025-07-08 20:49:08 -0400

    Update __init__.py

--- family_tree/domain/services/__init__.py
+++ family_tree/domain/services/__init__.py
@@ -1,4 +1,4 @@
-
+# family_tree/domain/services/__init__.py
 from family_tree.app.extensions import db
 from family_tree.domain.models.person import Person
 from family_tree.domain.services.person_service import PersonService
