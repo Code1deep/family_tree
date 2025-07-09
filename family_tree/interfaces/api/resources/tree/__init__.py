957185a24772dcb0bdda7d350e747c6151682474
Code1deep <az.bahya@gmail.com>
2025-07-08 20:08:54 -0400

    Update __init__.py

--- family_tree/interfaces/api/resources/tree/__init__.py
+++ family_tree/interfaces/api/resources/tree/__init__.py
@@ -1,5 +1,5 @@
 # interfaces/api/resources/tree/__init__.py
 
-from .init_tree_service import init_tree_resources
+from family_tree.interfaces.api.resources.tree.init_tree_service import init_tree_resources
 
 
