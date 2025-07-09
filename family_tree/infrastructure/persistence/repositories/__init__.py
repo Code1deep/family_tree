9fdb91574025ac0d1578769426b6f8428542938b
Code1deep <az.bahya@gmail.com>
2025-07-08 20:51:37 -0400

    Update __init__.py

--- family_tree/infrastructure/persistence/repositories/__init__.py
+++ family_tree/infrastructure/persistence/repositories/__init__.py
@@ -1,4 +1,4 @@
-
+# family_tree/infrastructure/persistence/repositories/__init__.py
 from family_tree.infrastructure.persistence.repositories.person_repo import PersonRepository
 
 __all__ = ['PersonRepository']
