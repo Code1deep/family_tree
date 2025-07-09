c55bf3fd39281eb3a94d89ed50b24471b9cdca28
Code1deep <az.bahya@gmail.com>
2025-07-08 20:47:36 -0400

    Update __init__.py

--- family_tree/domain/models/__init__.py
+++ family_tree/domain/models/__init__.py
@@ -1,9 +1,4 @@
-
-
-#from flask_sqlalchemy import SQLAlchemy
-
-#db = SQLAlchemy()
-
+# family_tree/domain/models/__init__.py
 from family_tree.domain.models.person import Person  # si tu as une classe Person dans person.py
 
 __all__ = ["Person"]
