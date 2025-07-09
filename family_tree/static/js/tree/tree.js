ed0be7f80203047316d07d514062e42962444362
Code1deep <az.bahya@gmail.com>
2025-07-08 22:39:06 -0400

    Update tree.js

--- family_tree/static/js/tree/tree.js
+++ family_tree/static/js/tree/tree.js
@@ -52,8 +52,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   try {
-    console.log("📡 Fetch vers /api/person/tree...");
-    const response = await fetch("/api/person/tree");
+    console.log("📡 Fetch vers /api/tree...");
+    const response = await fetch("/api/tree");
     if (!response.ok) throw new Error(`HTTP ${response.status}`);
     const treeData = await response.json();
     console.log("✅ Données reçues :", treeData);
