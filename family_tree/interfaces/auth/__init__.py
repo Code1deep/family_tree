41f2d6258ebbdb0674d11d2eafe99beb462ff0e0
Code1deep <az.bahya@gmail.com>
2025-07-08 20:18:10 -0400

    Update __init__.py

--- family_tree/interfaces/auth/__init__.py
+++ family_tree/interfaces/auth/__init__.py
@@ -1,3 +1,3 @@
 # interfaces/api/auth/__init__.py
-from .routes import auth_bp
-from .user import load_user
+from family_tree.interfaces.auth.routes import auth_bp
+from family_tree.interfaces.auth.user import load_user
