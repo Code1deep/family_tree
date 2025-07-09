d261e2cc071e178e21bab0fe2cd39f327171ac22
Code1deep <az.bahya@gmail.com>
2025-07-08 20:14:57 -0400

    Update __init__.py

--- family_tree/interfaces/api/serializers/__init__.py
+++ family_tree/interfaces/api/serializers/__init__.py
@@ -1,5 +1,5 @@
 
 
-from interfaces.api.serializers.person_serializer import PersonSerializer  # Export unique
+from family_tree.interfaces.api.serializers.person_serializer import PersonSerializer  # Export unique
 
 __all__ = ['PersonSerializer']  # Rend explicite ce qui est exposé
